c4f87f7746467c1226186e3c2ddb31349c18b3f0
Jan Geuss Popelka <jan.popelka@firma.seznam.cz>
2022-01-17 22:33:29 +0100

    refactor: simplify terminal usage

--- src/main.rs
+++ src/main.rs
@@ -1,154 +1,60 @@
-use bevy::{
-    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
-    prelude::*,
-    window::WindowResized,
-};
+use bevy::diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin};
+use bevy::prelude::*;
 
 extern crate rand;
 
 pub mod terminal;
-use terminal::Terminal;
-
-//const FONT_PATH: &str = "fonts/DejaVuSansMono-Bold.ttf";
-const FONT_PATH: &str = "fonts/FreeMonoBold.otf";
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-enum TerminalState {
-    New,
-    Resized,
-    Ready,
-}
+use terminal::TerminalPlugin;
+use terminal::TerminalEvent;
+use crate::terminal::Terminal;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(TerminalPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .insert_resource(ClearColor(Color::BLACK))
-        .add_startup_system(setup)
-        .add_state(TerminalState::New)
-        .add_system(window_resized_system)
-        .add_system(scale_terminal_system)
-        .add_system(text_update_system)
-        .add_system(text_color_system)
+        .add_system(display_fps_system)
+        .add_system(random_text_system)
         .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
 
-#[derive(Component)]
-struct FpsText;
-
-#[derive(Component)]
-struct Foreground;
-
-#[derive(Component)]
-struct Background;
-
-fn window_resized_system(
-    mut event_resized: EventReader<WindowResized>,
-    mut state: ResMut<State<TerminalState>>,
-) {
-    for _event in event_resized.iter() {
-        if state.current() == &TerminalState::Ready {
-            state.set(TerminalState::New).unwrap();
-        }
-        break;
-    }
-}
-
-fn scale_terminal_system(
-    mut commands: Commands,
-    query: Query<Entity, Or<(With<Foreground>, With<Background>)>>,
-    asset_server: Res<AssetServer>,
-    terminal: Option<Res<Terminal>>,
-    text: Query<&CalculatedSize, With<Foreground>>,
-    mut state: ResMut<State<TerminalState>>,
-    windows: Res<Windows>,
-) {
-    let mut new_state = state.current().clone();
-    let font_scale: (f32, f32) = match state.current() {
-        TerminalState::New => {
-            new_state = TerminalState::Resized;
-            (1.0, 1.0)
-        }
-        TerminalState::Resized => {
-            let size = text.single();
-            let terminal = terminal.unwrap();
-            if size.size.width > 0.0 && size.size.height > 0.0 {
-                new_state = TerminalState::Ready;
-                (
-                    size.size.width / terminal.cols as f32 / terminal.font_size,
-                    size.size.height / terminal.rows as f32 / terminal.font_size,
-                )
-            } else {
-                (1.0, 1.0)
-            }
-        }
-        TerminalState::Ready => terminal.unwrap().font_scale
-    };
-
-     if state.current() != &new_state {
-        query.iter().for_each(|id| commands.entity(id).despawn());
-        let window = windows.get_primary().unwrap();
-        let width = window.width();
-        let height = window.height();
-
-        let resized_terminal = Terminal::new(
-            width, height, FONT_PATH.to_string(), font_scale
-        );
-        let fg = resized_terminal.create_layer(width, height, &asset_server);
-        let bg = resized_terminal.create_layer(width, height, &asset_server);
-        commands.insert_resource(resized_terminal);
-        commands.spawn_bundle(bg).insert(Background);
-        commands.spawn_bundle(fg).insert(Foreground);
-        //println!("scale_terminal_system {:?} -> {:?} font_scale = {:?}, w = {}, h = {}", state.current(), new_state, font_scale, width, height);
-        state.set(new_state).unwrap();
-    }
-
-}
-
-fn setup(mut commands: Commands) {
-    commands.spawn_bundle(UiCameraBundle::default());
-}
-
-fn text_update_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text, With<FpsText>>) {
-    for mut text in query.iter_mut() {
-        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
-            if let Some(average) = fps.average() {
-                // Update the value of the second section
-                text.sections[1].value = format!("{:.0}", average);
-            }
+fn display_fps_system(diagnostics: Res<Diagnostics>, mut events: EventWriter<TerminalEvent>) {
+    if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+        if let Some(average) = fps.average() {
+            events.send(TerminalEvent {
+                row: 0, col: 0, s: format!("{:.0} fps", average), attr: (Color::WHITE, Color::BLACK)
+            });
         }
     }
 }
 
-fn text_color_system(time: Res<Time>,
-     terminal: Option<Res<Terminal>>,
-     mut foreground: Query<&mut Text, (With<Foreground>, Without<Background>)>,
-     mut background: Query<&mut Text, (With<Background>, Without<Foreground>)>,
-) {
-    if let (Some(terminal) , Some(mut fore), Some(mut back)) =
-        (terminal, foreground.iter_mut().next(), background.iter_mut().next()) {
-        let k = rand::random::<usize>() % 52;
-        let row = rand::random::<usize>() % terminal.rows;
-        let col = rand::random::<usize>() % terminal.cols;
+fn random_text_system(time: Res<Time>, terminal: Query<&Terminal>, mut events: EventWriter<TerminalEvent>) {
+
+    if let Some(x) = terminal.iter().next() {
+        let seconds = time.seconds_since_startup() as f32;
+
+        let (rows, cols) = x.getmaxxy();
+
+        let row = 1 + rand::random::<usize>() % (rows - 1);
+        let col = rand::random::<usize>() % cols;
+
         const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        if let Some(c) = ALPHABET.chars().nth(k) {
-            let seconds = time.seconds_since_startup() as f32;
-            let fg = Color::Rgba {
-                red: (1.25 * seconds).sin() / 2.0 + 0.5,
-                green: (0.75 * seconds).sin() / 2.0 + 0.5,
-                blue: (0.50 * seconds).sin() / 2.0 + 0.5,
-                alpha: 1.0,
-            };
-            let bg = Color::Rgba {
-                red: 1.0 - fg.r(),
-                green: 1.0 - fg.g(),
-                blue: 1.0 - fg.b(),
-                alpha: 1.0,
-            };
-            terminal
-                .with(fore.as_mut(), back.as_mut())
-                .mvaddch(row, col, c, &fg, &bg);
-        }
+        let k = rand::random::<usize>() % ALPHABET.chars().count();
+        let c = ALPHABET.chars().nth(k).unwrap();
+
+        let fg = Color::Rgba {
+            red: (1.25 * seconds).sin() / 2.0 + 0.5,
+            green: (0.75 * seconds).sin() / 2.0 + 0.5,
+            blue: (0.50 * seconds).sin() / 2.0 + 0.5,
+            alpha: 1.0,
+        };
+        let bg = Color::Rgba {
+            red: 1.0 - fg.r(),
+            green: 1.0 - fg.g(),
+            blue: 1.0 - fg.b(),
+            alpha: 1.0,
+        };
+        events.send(TerminalEvent{ row, col, s: c.to_string(), attr: (fg, bg, ) });
     }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,47 +1,169 @@
 use bevy::prelude::*;
+use bevy::window::WindowResized;
 
+#[derive(Component)]
 pub struct Terminal {
     pub rows: usize,
     pub cols: usize,
     pub font_size: f32,
     pub font_path: String,
     pub font_scale: (f32, f32),
+    pub color_pair: (Color, Color),
 }
 
-pub struct Ncurses<'a> {
-    terminal: &'a Terminal,
-    foreground: &'a mut Text,
-    background: &'a mut Text,
+//const FONT_PATH: &str = "fonts/DejaVuSansMono-Bold.ttf";
+const FONT_PATH: &str = "fonts/FreeMonoBold.otf";
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum TerminalState {
+    New,
+    Resized,
+    Ready,
 }
 
-impl<'a> Ncurses<'a> {
-    pub fn mvaddch(&'a mut self, row: usize, col: usize, c: char, fg: &Color, bg: &Color) {
-        let idx = (self.terminal.cols + 1) * row + col;
-        let section = &mut self.foreground.sections[idx];
-        section.value = String::from(c);
-        section.style.color = *fg;
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(UiCameraBundle::default());
+}
 
-        let section = &mut self.background.sections[idx];
-        section.value = String::from('█');
-        section.style.color = *bg;
+pub struct TerminalEvent {
+    pub row: usize,
+    pub col: usize,
+    pub s: String,
+    pub attr: (Color, Color)
+}
 
+impl TerminalEvent {
+    pub fn attron(attr: (Color, Color)) -> Self {
+        Self { row: 0, col: 0, s: " ".to_string(), attr }
+    }
+
+    pub fn mvaddch(row: usize, col: usize, c: char) -> Self {
+        Self { row, col, s: c.to_string(), attr: (Color::WHITE, Color::BLACK) }
+    }
+}
+pub struct TerminalOperation(pub Vec<TerminalEvent>);
+
+#[derive(Component)]
+struct Foreground;
+
+#[derive(Component)]
+struct Background;
+
+fn window_resized_system(
+    mut event_resized: EventReader<WindowResized>,
+    mut state: ResMut<State<TerminalState>>,
+) {
+    for _event in event_resized.iter() {
+        if state.current() == &TerminalState::Ready {
+            state.set(TerminalState::New).unwrap();
+        }
+        break;
+    }
+}
+
+fn scale_terminal_system(
+    mut commands: Commands,
+    query: Query<Entity, Or<(With<Foreground>, With<Background>)>>,
+    asset_server: Res<AssetServer>,
+    mut terminal: Query<(Entity, &Terminal)>,
+    text: Query<&CalculatedSize, With<Foreground>>,
+    mut state: ResMut<State<TerminalState>>,
+    windows: Res<Windows>,
+) {
+    let mut new_state = state.current().clone();
+    let font_scale: (f32, f32) = match state.current() {
+        TerminalState::New => {
+            new_state = TerminalState::Resized;
+            (1.0, 1.0)
+        }
+        TerminalState::Resized => {
+            let size = text.single();
+            let terminal = terminal.single_mut().1;
+            if size.size.width > 0.0 && size.size.height > 0.0 {
+                new_state = TerminalState::Ready;
+                (
+                    size.size.width / terminal.cols as f32 / terminal.font_size,
+                    size.size.height / terminal.rows as f32 / terminal.font_size,
+                )
+            } else {
+                (1.0, 1.0)
+            }
+        }
+        TerminalState::Ready => terminal.single_mut().1.font_scale
+    };
+
+    if state.current() != &new_state {
+        query.iter().for_each(|id| commands.entity(id).despawn());
+        let window = windows.get_primary().unwrap();
+        let width = window.width();
+        let height = window.height();
+
+        let resized_terminal = Terminal::new(
+            width, height, FONT_PATH.to_string(), font_scale
+        );
+        if let Some(old_terminal) = terminal.iter_mut().next() {
+            commands.entity(old_terminal.0).despawn();
+        }
+        let back = resized_terminal.create_layer(width, height, &asset_server);
+        let fore = resized_terminal.create_layer(width, height, &asset_server);
+        commands.spawn_bundle(back).insert(Background);
+        commands.spawn_bundle(fore).insert(Foreground);
+        commands.spawn().insert(resized_terminal);
+        state.set(new_state).unwrap();
+    }
+
+}
+
+pub struct TerminalPlugin;
+
+impl Plugin for TerminalPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(ClearColor(Color::BLACK))
+            .add_startup_system(setup)
+            .add_state(TerminalState::New)
+            .add_event::<TerminalEvent>()
+            .add_system(window_resized_system)
+            .add_system(scale_terminal_system)
+            .add_system_set(SystemSet::on_update(TerminalState::Ready)
+                .with_system(terminal_update_system));
+    }
+}
+
+fn terminal_update_system(mut q0: Query<&mut Terminal>, mut q1: Query<&mut Text, (With<Foreground>, Without<Background>)>,
+        mut q2: Query<&mut Text, (With<Background>, Without<Foreground>)>, mut events: EventReader<TerminalEvent>) {
+    for e in events.iter() {
+        let mut terminal = q0.single_mut();
+        let mut fore = q1.single_mut();
+        let mut back = q2.single_mut();
+        terminal.attron(e.attr);
+        terminal.mvprintw(fore.as_mut(), back.as_mut(), e.row, e.col, &e.s);
     }
 }
 impl Terminal {
 
+    pub fn getmaxxy(&self) -> (usize, usize) {
+        (self.rows, self.cols)
+    }
+
+    pub fn command(&mut self, fun: fn(terminal: &mut Self, fore: &mut Text, back: &mut Text) -> (),
+                   fore: &mut Text, back: &mut Text) {
+        fun(self, fore, back);
+    }
     pub fn new(width: f32, height: f32, font_path: String, font_scale: (f32, f32)) -> Self {
         const Y: usize = 25;
         const X: usize = 80;
 
         let _y = height / Y as f32;
         let _x = width / X as f32;
-        let font_size = if _x/(X as f32)>= _y/(Y as f32) { _x / font_scale.0 } else { _y / font_scale.1 };
+        let font_size = if _x >= _y { _x / font_scale.0 } else { _y / font_scale.1 };
         let rows = std::cmp::max(1, (height / font_size / font_scale.1).floor() as usize);
         let cols = std::cmp::max(1, (width / font_size / font_scale.0).floor() as usize);
-        Self {rows, cols, font_size, font_scale, font_path}
+
+        Self {rows, cols, font_size, font_scale, font_path, color_pair: (Color::WHITE, Color::BLACK)}
     }
 
-    pub fn create_layer(&self, width: f32, height: f32, asset_server: &AssetServer) -> TextBundle {
+    fn create_layer(&self, width: f32, height: f32, asset_server: &AssetServer) -> TextBundle {
         let off_x = (width - self.cols as f32 * self.font_size * self.font_scale.0) / 2.;
         let off_y = (height - self.rows as f32 * self.font_size * self.font_scale.1) / 2.;
 
@@ -83,7 +205,32 @@ impl Terminal {
         }
     }
 
-    pub fn with<'a>(&'a self, foreground: &'a mut Text, background: &'a mut Text) -> Ncurses<'a> {
-        Ncurses { terminal: self, foreground, background }
+    pub fn attron(&mut self, color_pair: (Color, Color)) {
+        self.color_pair = color_pair;
     }
+
+    pub fn mvaddch(&self, fore: &mut Text, back: &mut Text, row: usize, col: usize, c: char) {
+        let idx = (self.cols + 1) * row + col;
+        fn addch(section: &mut TextSection, c: char, col: Color) {
+            section.value = String::from(c);
+            section.style.color = col;
+        }
+        if (idx < back.sections.len()) & (idx < fore.sections.len()) {
+            addch(&mut back.sections[idx], '█', self.color_pair.1);
+            addch(&mut fore.sections[idx], c, self.color_pair.0);
+        }
+    }
+
+    pub fn mvprintw(&mut self, fore: &mut Text, back: &mut Text, row: usize, col: usize, s: &str) {
+        let mut cur: (usize, usize) = (row, col);
+        for c in s.chars() {
+            self.mvaddch(fore, back, cur.0, cur.1, c);
+            cur.1 += 1;
+            if cur.1 >= self.cols {
+                cur.0 += 1;
+                cur.1 = 0;
+            }
+        }
+    }
+
 }
