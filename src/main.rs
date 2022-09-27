af37ba999c530f2d5de8962898c1caee21ee8aad
Jan Popelka <popojan@protonmail.com>
2022-09-27 22:58:45 +0200

    menu navigation, delete obsolete function

--- src/main.rs
+++ src/main.rs
@@ -187,36 +187,6 @@ fn _display_fps_system(diagnostics: Res<Diagnostics>, mut events: EventWriter<Te
     }
 }
 
-fn _random_text_system(time: Res<Time>, terminal: Query<&Terminal>, mut events: EventWriter<TerminalEvent>) {
-
-    if let Some(x) = terminal.iter().next() {
-        let seconds = time.seconds_since_startup() as f32;
-
-        let (rows, cols) = x.getmaxxy();
-
-        let row = 1 + rand::random::<usize>() % (rows - 1);
-        let col = rand::random::<usize>() % cols;
-
-        const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let k = rand::random::<usize>() % ALPHABET.chars().count();
-        let c = ALPHABET.chars().nth(k).unwrap();
-
-        let fg = Color::Rgba {
-            red: (1.25 * seconds).sin() / 2.0 + 0.5,
-            green: (0.75 * seconds).sin() / 2.0 + 0.5,
-            blue: (0.50 * seconds).sin() / 2.0 + 0.5,
-            alpha: 1.0,
-        };
-        let bg = Color::Rgba {
-            red: 1.0 - fg.r(),
-            green: 1.0 - fg.g(),
-            blue: 1.0 - fg.b(),
-            alpha: 1.0,
-        };
-        events.send(TerminalEvent{ row, col, s: c.to_string(), attr: (fg, bg, ) });
-    }
-}
-
 fn grammar_derivation_system(time_step_code: KeyCode,
                              mut commands: Commands,
                              program_file: Res<ProgramFile>,
